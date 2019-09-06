4bcb1dead0bb9eb02d57b06dc3d895cd43a57053
Sushma-148 <54669945+Sushma-148@users.noreply.github.com>
2019-09-06 17:13:13 +0530

    Update and rename .gitignore to add2num.C

---
+++ .gitignore/add2num.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int input()
+{
+ int a;
+ printf("enter the number\n");
+ scanf("%d",&a);
+ return a;
+ }
+ int add(int a,int b)
+ {
+ return a+b;
+ }
+ void output(int a,int b,int c)
+ {
+ printf("%d+%d=%d\n",a,b,c);
+ }
+ int main()
+ {
+ int a,b,c;
+ a=input();
+ b=input();
+ c=add(a,b);
+ output(a,b,c);
+ }
